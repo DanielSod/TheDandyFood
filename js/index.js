e324d783ec48cfda26e8103d0d36c62eba3aaded
Daniel Söderberg <dansod85@gmail.com>
2021-01-27 14:36:01 +0100

    setting up project

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
-﻿let server = require('../Server/Server.js')
+﻿//let server = require('../Server/Server.js')
+const express = require('express');
+const Datastore = require('nedb');
+const app = express();
+app.listen(5500, () => console.log('listening at 5500'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('dandyfood.db');
+database.loadDatabase();
+database.insert({
+    Username: 'DandyThree',
+    Password: 'dthree',
+    Realname: 'Dandy Try Three',
+    Email: 'dthree@dandy.se',
+    Country: 'Dandyland',
+});
+
+
+app.post('/api', (request, response) => {
+    console.log('I Got A Request');
+    console.log(request.body);
+    const data = request.body;
+    database.push(data);
+    console.log(database);
+    response.json({
+        status: 'success',
+        latitude: data.lat,
+        longitude: data.lon
+    });
+});
--- js/main.js
+++
@@ -1,6 +0,0 @@
-﻿document.getElementById("button").addEventListener('click', function () {
-    alert("En ruta för att bygga nytt formulär? Hej");
-});
-
-
-
