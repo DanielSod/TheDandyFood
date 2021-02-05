980112456ee3d6e5e87c2f8705cd9b6f53e22224
AndyCarlsson <andreas95carlsson@gmail.com>
2021-02-05 16:27:18 +0100

    Updated search

--- public/js/mapview.js
+++ public/js/mapview.js
@@ -1,38 +1,39 @@
 // getRecipeData();
 // getUser();
 
-let userArray = [];
 
-async function getRecipeData() {
-  console.log('Trying to get data');
 
-  const response = await fetch('/api');
-  userArray = await response.json();
-  console.log(userArray);
+// async function getRecipeData() {
+//   console.log('Trying to get data');
 
-  let container = document.querySelector('.recipe-card-container');
+//   const response = await fetch('/api');
+//   userArray = await response.json();
+//   console.log(userArray);
 
-  for (let i = 0; i < userArray.length; i++) {
-    container.innerHTML +=
-      '<div class="recipe-card"><div>Bild</div><div class="description-container"><h1>' +
-      userArray[i].Realname +
-      '</h1><p>' +
-      userArray[i].Username +
-      '</p></div></div>';
-  }
-}
+//   let container = document.querySelector('.recipe-card-container');
+
+//   for (let i = 0; i < userArray.length; i++) {
+//     container.innerHTML +=
+//       '<div class="recipe-card"><div>Bild</div><div class="description-container"><h1>' +
+//       userArray[i].Realname +
+//       '</h1><p>' +
+//       userArray[i].Username +
+//       '</p></div></div>';
+//   }
+// }
 
 async function getUser(){
-
-let searchTerm = document.querySelector("#recipe-search").value;
-console.log(searchTerm);
+  let container = document.querySelector('.recipe-card-container');
+  let userArray = [];
+  container.innerHTML = "";
+  let searchTerm = document.querySelector("#recipe-search").value;
 
  const response = await fetch(`/users/${searchTerm}`);
 
  const data = await response.json();
 
  userArray = data;
- let container = document.querySelector('.recipe-card-container');
+ 
  for (let i = 0; i < userArray.length; i++) {
   container.innerHTML +=
     '<div class="recipe-card"><div>Bild</div><div class="description-container"><h1>' +
