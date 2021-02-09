a4c741c06d66ff33a5e85545d933487dff902dd5
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-02-09 15:20:42 +0100

    Added check for Username / email for signup

---
+++ public/js/signup.js
@@ -0,0 +1,31 @@
+async function createUser() {
+  const user = {
+    Username: document.querySelector('#edit-username').value,
+    Password: document.querySelector('#edit-password').value,
+    Realname: document.querySelector('#edit-name').value,
+    Email: document.querySelector('#edit-email').value,
+    Country: document.querySelector('#edit-country').value,
+    Favorites: [],
+  };
+
+  const response = await fetch(`/users`);
+  const dbUsers = await response.json();
+
+  for (const i in dbUsers) {
+    if (user.Username == dbUsers[i].Username || user.Email == dbUsers[i].Email) {
+      console.log('Username or Email already exists');
+      break;
+    }
+    if (i == dbUsers.length - 1) {
+      const options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      };
+      fetch('/api', options);
+      console.log('User Created');
+    }
+  }
+}
