3ef51f653958e9ccbece1f0d0af7a7bac0f977b8
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-02-12 11:46:28 +0100

    Added modal for log in

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,33 @@
-const loginForm = document.querySelector('.login-form');
-const loginButton = document.querySelector('#loginSubmit');
+// window.onload = function () {
+//   const loginForm = document.querySelector('.login-form');
+//   const loginButton = document.querySelector('#loginSubmit');
 
-loginButton.addEventListener('click', async (e) => {
-  e.preventDefault();
+//   if (loginButton != null) {
+//     loginButton.addEventListener('click', async (e) => {
+//       e.preventDefault();
+
+//       console.log('Hej');
+
+//       const username = loginForm.username.value;
+//       const password = loginForm.password.value;
+
+//       const response = await fetch(`/users`);
+
+//       const users = await response.json();
+
+//       users.forEach((user) => {
+//         if (username === user.Username && password === user.Password) {
+//           localStorage.setItem('loggedInUser', JSON.stringify(user));
+//         } else {
+//           console.log('Wrong Username / Password combination');
+//         }
+//       });
+//     });
+//   }
+// };
+
+async function submitClicked() {
+  const loginForm = document.querySelector('.login-form');
 
   const username = loginForm.username.value;
   const password = loginForm.password.value;
@@ -10,6 +35,7 @@ loginButton.addEventListener('click', async (e) => {
   const response = await fetch(`/users`);
 
   const users = await response.json();
+  console.log(users);
 
   users.forEach((user) => {
     if (username === user.Username && password === user.Password) {
@@ -18,4 +44,4 @@ loginButton.addEventListener('click', async (e) => {
       console.log('Wrong Username / Password combination');
     }
   });
-});
+}
