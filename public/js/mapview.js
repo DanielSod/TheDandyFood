61e5a24758807cadae022009362fe40473450d42
Andreas Carlsson <andreas95carlsson@gmail.com>
2021-02-14 17:53:44 +0100

    Updated Navigation via reciep from mapview to recipeDetail

--- public/js/mapview.js
+++ public/js/mapview.js
@@ -1,9 +1,9 @@
+let recipeList = [];
+
 async function getRecipes() {
   let container = document.querySelector('.recipe-card-container');
   container.innerHTML = '';
   let searchTerm = document.querySelector('#recipe-search').value;
-
-  var recipeList = [];
 
   const response = await fetch(`/recipes/${searchTerm}`);
 
@@ -22,18 +22,18 @@ async function getRecipes() {
   }
 
   for (let i = 0; i < container.children.length; i++) {
-    (function (i) {
-      console.log('Inside func');
-      container.children[i].onclick = function () {
-        console.log(i);
-      };
-    });
+    container.children[i].onclick = function () {
+      console.log(i);
+      setRecipe(i);
+    };
   }
 }
 
-function setRecipe() {
+function setRecipe(i) {
+  id = recipeList[i]._id;
   let url = new URL('http://localhost:5500/recipeDetail.html');
   url.searchParams.append('id', id);
 
-  // document.location.href = url;
+  console.log(id);
+  document.location.href = url;
 }
