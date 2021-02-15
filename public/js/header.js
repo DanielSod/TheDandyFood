2e6b58769d1b5f841f22c156de2eb0cfb188bced
AndyCarlsson <andreas95carlsson@gmail.com>
2021-02-15 15:48:45 +0100

    Updated log in component

--- public/js/header.js
+++ public/js/header.js
@@ -1,12 +1,19 @@
-getHeader();
 
-function getHeader(){
-    fetch("./header.html")
-    .then(response => {
-      return response.text()
-    })
-    .then(data => {
-      document.querySelector(".nav-container").innerHTML = data;
-    });
+window.onload = function(){
+
+  getHeader();
+  setLoggedInStatus();
+  function getHeader(){
+      fetch("./header.html")
+      .then(response => {
+        return response.text()
+      })
+      .then(data => {
+        document.querySelector(".nav-container").innerHTML = data;
+      });
+  }
+
 }
+
+
 
