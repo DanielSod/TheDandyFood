ff7403cf3ddbe0a272028feecf0458a6271ef9d1
Daniel Söderberg <dansod85@gmail.com>
2021-02-17 09:54:54 +0100

    addRecipe minor updates, not ready

--- public/js/addrecipe.js
+++ public/js/addrecipe.js
@@ -1,5 +1,6 @@
-//let server = require('../Server/Server.js')
+const { table } = require("console");
 
+//let server = require('../Server/Server.js')
 async function uploadrecipe() {
   console.log('Data sent');
 
@@ -17,7 +18,6 @@ async function uploadrecipe() {
     PortionSize: document.querySelector("#portionSize").value,
     CookTime: document.querySelector("#cookTime").value,
     Signature: document.querySelector("#signature").value,
-
     RecPic: document.querySelector("#recPic").value,
     Difficulty: document.querySelector("#difficulty").value,
     HowTo: document.querySelector("#howTo").value,
@@ -25,37 +25,12 @@ async function uploadrecipe() {
     Unit: document.querySelector("#unit").value,
     Qty: document.querySelector("#qty").value
   };
+  addEventListener
 
   const data = new Object(recipe);
   JSON.stringify(recipe);
   console.log(data)
 }
-
-function getAllIngredients() {
-  let table = document.querySelectorAll('#addIngredientItem');
-  var colQty = document.querySelectorAll('.colQty')
-  var colUnit = document.querySelectorAll('.colUnit')
-  var colIngred = document.querySelectorAll('.colIngred')
-
-  console.log(table);
-}
-
-async function addIngredient() {
-let table = document.querySelector('#addIngredientItem')
-
-table.innerHTML +=
-    '<tr><td><input id="qty"type="text"class="colQty"></td><td><input id="unit" type="text"class="colUnit"></td><td><input id="ingredient" type="text"class="colIngred"></td></tr>'
-};
-
-
-postRecipe(() => '/recipes', function()  {
-  recipe: request.body,
-  newrecipe._id = Math.floor(Date.now() / 1000);
-  recipesDB.insert( newrecipe, function ( err, recipes) {
-    if ( err ) response.status( 500 ).send( err );
-    else response.sendStatus( 200 );
-  });
-});
 
 function picture() {
   '#thumbnail'.on('change', function () {
@@ -69,9 +44,71 @@ function picture() {
   });
 }
 
-async function getfromDB() {
-  const response = await fetch(`/recipe/${recipeTemp.Title[1]}`);
-  const data = await response.json();
+ TODO//--!!!:: //Rör ej koden ovanför här ::!!!--TODO
+//_____________________________________________________
 
-  console.log(data);
+
+TODO //ingredientArray och How To Arraya
+
+function createIngredientArr() {
+  let ingredientArray = [];
+  this.Ingredient.ingredientArray.append(Ingredient);
+  ingredientArray = document.querySelector('#ingredientTable')
+
+  ingredientArray.innerHtml +=
+  '<td><input id="qty" type="text" class="colqty"></td><td><input id="unit" type="text" class="colunit"></td><td><input id="ingredients" type="text" class="colingred"></td>';
+};
+
+
+
+function addHowToCookRows() {
+  let howToArr = [];
+
+howToArr.innerHtml +=
+    '<tr><td><input type="text" id="howTo"></input></td></tr>'};
+
+
+function displayIngredientArr() {
+  return ingredientArray;
 }
+
+function displayHowToArr() {
+  return ingredientArray;
+}
+
+
+// TODO: addIngredient
+//  async function addIngredient() {
+// let table = document.querySelector('#addIngredientItem')
+
+// table.innerHTML +=
+//     '<tr><td><input id="qty"type="text"class="colQty"></td><td><input id="unit" type="text"class="colUnit"></td><td><input id="ingredient" type="text"class="colIngred"></td></tr>'
+// };
+
+// TODO: showIngredientList && howToList
+// function getAllIngredients() {
+//   let table = document.querySelectorAll('#addIngredientItem');
+//   var colQty = document.querySelectorAll('.colQty')
+//   var colUnit = document.querySelectorAll('.colUnit')
+//   var colIngred = document.querySelectorAll('.colIngred')
+
+//   console.log(table);
+// }
+
+// TODO: postRecipe
+// postRecipe(() => '/recipes', function()  {
+//   recipe: request.body,
+//   newrecipe._id = Math.floor(Date.now() / 1000);
+//   recipesDB.insert( newrecipe, function ( err, recipes) {
+//     if ( err ) response.status( 500 ).send( err );
+//     else response.sendStatus( 200 );
+//   });
+// });
+
+
+// async function getfromDB() {
+//   const response = await fetch(`/recipe/${recipeTemp.Title[1]}`);
+//   const data = await response.json();
+
+//   console.log(data);
+// }
